c7ed11699f43d88be58b8f116d3d98bc6d409f6f
Vovusik <vovaandrukhiv@gmail.com>
2018-12-13 15:29:56 +0200

    Добавив можливість отримувати додатком сповіщень, відправлених з консолі firebase

--- app/src/main/java/com/andrukhiv/mynavigationdrawer/AppController.java
+++ app/src/main/java/com/andrukhiv/mynavigationdrawer/AppController.java
@@ -1,6 +1,10 @@
 package com.andrukhiv.mynavigationdrawer;
 
 import android.app.Application;
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.content.Context;
+import android.os.Build;
 import android.support.v7.app.AppCompatDelegate;
 
 import com.andrukhiv.mynavigationdrawer.database.DbAdapter;
@@ -17,6 +21,23 @@ public class AppController extends Application {
         mDbHelper = DbAdapter.getInstance(getApplicationContext());
         mDbHelper.createDatabase();
         mDbHelper.open();
+
+        createChannel(); // Створюю канал для Notification
+    }
+
+    // todo: перевірку працездатності Notification не зміг, так як консоль firebase 1 год. відправляла сповіщення
+    private void createChannel() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            NotificationManager nm = (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
+            if (nm != null) {
+                NotificationChannel channel = new NotificationChannel(
+                        "Message",
+                        "title",
+                        NotificationManager.IMPORTANCE_HIGH);
+                nm.createNotificationChannel(channel);
+
+            }
+        }
     }
 
     // за замовчуванням автоматичний вибір теми додатку в залежності від часу доби
